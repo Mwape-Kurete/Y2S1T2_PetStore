e08b59ca21ef3beb7fa53497bb68d098f1e27e05
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-05-27 12:14:09 +0200

    backend
    backend environment created

--- cheeri/mern-backend/Server.js
+++ cheeri/mern-backend/Server.js
@@ -16,3 +16,11 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
